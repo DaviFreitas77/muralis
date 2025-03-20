fe2b2ac0e2f55fc2bf63380e2f03ce49dfd060b8
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2025-03-20 18:15:41 -0300

    add repository cliente

---
+++ src/main/java/com/comercio/comercio/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.comercio.comercio.entity.Cliente;
+
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
